e65dd8cf4963a24b18ab938345083606b316c362
elbasedd <baseddzz@gmail.com>
2026-02-02 18:39:51 +0100

    moved all links to their own js file, added sidebar, restyled a bit of everything, added new waw art cover; other js QoL additions, especially for asset loading

---
+++ builds.js
@@ -0,0 +1,405 @@
+const buildsData = {
+    mw2019: [
+        {
+            title: "Modern Warfare v1.67",
+            img: "images/mw2019/167.jpg",
+            tags: "Multiple sources",
+            links: [
+                { label: "Warzone Only | Google Drive", url: "https://drive.google.com/file/d/1gwV-dCMemctqn6OerhZoeEN8iZrDFV0O/view?usp=sharing" },
+                { label: "MP + Warzone (Steam) | Gofile", url: "https://gofile.io/d/QWpt1M" },
+                { label: "WZ Maps Only | Google Drive", url: "https://drive.google.com/file/d/1MdGbFUbK47juoTTv9Fx4VBtfntzVbZYi/view?usp=sharing" },
+                { label: "Battle.net Binaries | Google Drive", url: "https://drive.google.com/file/d/1k1hozdg1EAlaiLQ38ZI5N3mJuD41bFmg/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.64",
+            img: "images/mw2019/164.jpg",
+            tags: "BROKEN LINK · Full Build",
+            links: [
+                { label: "Download | Dropbox (BROKEN)", url: "https://www.dropbox.com/scl/fi/hy7yngvkss56l45y65d91/CodMW164.5.7z?dl=0" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.63",
+            img: "images/mw2019/163.jpg",
+            tags: "156GB · Full Build",
+            links: [
+                { label: "Google Drive", url: "https://drive.google.com/drive/folders/1QXzzGVOuHbIGTjKOsUjYlaSF33jFkGMB?usp=sharing" },
+                { label: "Buzzheavier (Faster)", url: "https://buzzheavier.com/d1z6oz6cb0ey" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.61",
+            img: "images/mw2019/161.jpg",
+            tags: "80GB · Warzone Only",
+            links: [
+                // { label: "Download | Buzzheavier", url: "https://buzzheavier.com/lfgww8y67uh9" },
+                { label: "Unavailable", url: "#", disabled: true }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.60",
+            img: "images/mw2019/160.jpg",
+            tags: "164GB · MP + WZ",
+            links: [
+                { label: "Warzone Only | Buzzheavier", url: "https://buzzheavier.com/lvyoprgngc2r" },
+                { label: "Full Build", url: "https://drive.google.com/file/d/1zfTyYyVPJSlOsfSKyeolKe3soNgLVgee/view?usp=sharing" },
+                { label: "Missing Files Addon | Google Drive", url: "https://drive.google.com/file/d/1g5fuVGcR5DY44G5PRXOnLIG7Ct0vCIXL/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.57",
+            img: "images/mw2019/157.jpg",
+            tags: "139GB · MP + WZ",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/1LVizOnBCcvE8CFMLfb_48RGwRvybnjsA/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.54",
+            img: "images/mw2019/154.jpg",
+            tags: "155GB · Full Build",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/drive/folders/1fyqRkxp4Zyyr9ryeBBk6hNK7EyzXkJVO?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.52",
+            img: "images/mw2019/152.jpg",
+            tags: "70GB · Full Build",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/1vq4ZSD9Hk1RFBSweXB7ko6IZsVwcsUUQ/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.46",
+            img: "images/mw2019/146.jpg",
+            tags: "77GB · Full Build",
+            links: [
+                { label: "Source 1 | Google Drive", url: "https://drive.google.com/drive/folders/1H_w1K2SMNON7p3QVbV4iDDOFpW1zNkQz?usp=sharing" },
+                { label: "Source 2 | Google Drive", url: "https://drive.google.com/file/d/1tO2smr_mq8xwdeOowT2cDgSW9nNuQVS8/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.45",
+            img: "images/mw2019/145.jpg",
+            tags: "80GB · Warzone Only",
+            links: [
+                { label: "Download | Gofile", url: "https://gofile.io/d/t2yL3V" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.44",
+            img: "images/mw2019/144.jpg",
+            tags: "81GB · Warzone Only",
+            links: [
+                { label: "Source 1 | Gofile", url: "https://gofile.io/d/f3ms9w" },
+                { label: "Source 2 | Google Drive", url: "https://drive.google.com/file/d/1BAk7wmEYCMmDn9bS1DPcR6V59FFOkIIJ/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.42",
+            img: "images/mw2019/142.png",
+            tags: "PC · Full Build",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/drive/folders/13pPhBcKQrdfGzIgpUf7ChQ8mi83uMyjc?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.41",
+            img: "images/mw2019/141.jpg",
+            tags: "202GB · Full Build",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/1cMVwjML_NNU9MPpr4jo6bFLdS5Vgb51u/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.39",
+            img: "images/mw2019/139.jpg",
+            tags: "Unavailable",
+            links: [
+                { label: "Unavailable", url: "#", disabled: true }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.38",
+            img: "images/mw2019/138.jpg",
+            tags: "200GB · Full Build",
+            links: [
+                { label: "Download | Gofile", url: "https://gofile.io/d/ISOvf8" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.37",
+            img: "images/mw2019/137.jpg",
+            tags: "200GB · Full Build",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/1ggjXS8ftRc_-u6mC2QfXBZXmK0eE-v0_/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.36",
+            img: "images/mw2019/136.jpg",
+            tags: "79GB · Full Build",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/1kpAqlOOvv06I7WBLg5jLAobKbx13k7vQ/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.34",
+            img: "images/mw2019/134.jpg",
+            tags: "73GB · Warzone Only",
+            links: [
+                { label: "Source 1 | Google Drive", url: "https://drive.google.com/file/d/1xkmFCnojegotOjlqttPTzq0Har5px8nY/view?usp=sharing" },
+                { label: "Source 2 | Google Drive", url: "https://drive.google.com/drive/folders/1-tWbkojBYVlQOQAfgObbVslydiJ4KRRF?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.31",
+            img: "images/mw2019/131.png",
+            tags: "Unavailable",
+            links: [
+                { label: "Unavailable", url: "#", disabled: true }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.30",
+            img: "images/mw2019/130.jpg",
+            tags: "136GB · MP + WZ",
+            links: [
+                { label: "WZ Only | Google Drive", url: "https://drive.google.com/file/d/1qtAQGKpnFSCeK32NcKyHdyNNp919Ln0a/view?usp=sharing" },
+                { label: "MP + WZ | Google Drive", url: "https://drive.google.com/file/d/1KgGGJ2b_iX_vUe4JJhBCwkjYhe1KYsxd/view?usp=drivesdk" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.28",
+            img: "images/mw2019/128.jpg",
+            tags: "104GB · Partially Corrupted",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/19Vm9_ETBsL45C1m3sEQVwCbIaHeINrsT/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.24",
+            img: "images/mw2019/124.jpg",
+            tags: "188GB · Full Build",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/drive/u/0/mobile/folders/1G33TVFYnMu1DeCFrSmzQLh15__84p2Yp/1bgyQYx5JmxGHSeGXyzxWMMeCF8gBPcag" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.23",
+            img: "images/mw2019/123.jpg",
+            tags: "92GB · Full Build",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/1psGkq4oD-YI2ylCKtadb0T-BPo_Z5CkZ/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.20",
+            img: "images/mw2019/120.jpg",
+            tags: "85GB · Some MP + WZ (Dev exe + Ship)",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/1sxjJs-GUISEmhnB1Nf6zaHXeC9m8KXRc/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.16",
+            img: "images/mw2019/116.jpg",
+            tags: "111GB · First WZ Build · Full Build",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/1jchB2MOTKPGNRJvFQALTVPyyPOtYGM1y/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare v1.03",
+            img: "images/mw2019/103.jpg",
+            tags: "103GB · First MP + WZ · Full Build",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/1fnvi2rfPhOB3cVPd2GrJXx8DkpX5pHa0/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "MW2019 Public Beta",
+            img: "images/mw2019/beta.jpg",
+            tags: "36GB · Beta",
+            links: [
+                { label: "Download | Google Drive", url: "https://drive.google.com/file/d/1yrBA5OI4SkzCQl3NVrTHook7M4ov8vBW/view?usp=sharing" }
+            ]
+        }
+    ],
+    bocw: [
+        {
+            title: "(Almost) Retail (1.34.0)",
+            img: "images/bocw/retail.jpg",
+            tags: "60GB · Retail",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1oY6BohvOlfpZ1Km0R0thrZLHlMVEVPBs/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Cold War Season 2",
+            img: "images/bocw/s2.jpg",
+            tags: "60GB · Windows 10 Only",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1XW1kM64UiCZ8EDIrw3Vgr6mD2aGjsvzI/view?usp=drivesdk" }
+            ]
+        },
+        {
+            title: "Cold War Public Beta",
+            img: "images/bocw/beta.jpg",
+            tags: "38GB · Beta",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1zHCvIGryzoZoTipoo6lBZD_3ObARQ3Fq/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Cold War Alpha",
+            img: "images/bocw/alpha.jpg",
+            tags: "100GB+ · Alpha",
+            links: [
+                { label: "Download", url: "https://drive.google.com/drive/folders/1GcUHUj5rkV_qVCkLg6PIQJ7NAqNmQ0OG" }
+            ]
+        }
+    ],
+    vg: [
+        {
+            title: "Retail (Latest, 1.26.1)",
+            img: "images/vg/retail.jpg",
+            tags: "89GB · Full Build",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1Xqzar-Sw8hfdL4FLKAZAFdYKG9bBUGgR/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Vanguard Public Beta",
+            img: "images/vg/beta.jpg",
+            tags: "15GB · Beta",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1ugmbKIdzQHE8AgFdmWWQ02N1YRxCxlKd/view?usp=sharing" }
+            ]
+        }
+    ],
+    mwii: [
+        {
+            title: "MWII Retail",
+            img: "images/mwii/retail.jpg",
+            tags: "86GB · Full Build",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1fIVd7EZ2py8K2-Vh2MJvBuDikRbRyj5j/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "MWII Early Access Campaign",
+            img: "images/mwii/ea.jpg",
+            tags: "PC · Campaign Only",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1zrkUtXsCkK72lXmRZDJHG3WBdwkj3Zy2/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "MWII Public Beta",
+            img: "images/mwii/beta.jpg",
+            tags: "19GB · Beta",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1Ma_7V5uwMmbKoxtTGhPwanU44UY2CTm7/view?usp=sharing" }
+            ]
+        }
+    ],
+    mwiii: [
+        {
+            title: "MWIII Retail",
+            img: "images/mwiii/retail.jpg",
+            tags: "142GB · Full Build",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1h5vaIlRL-ZlMIGbcGXC7m9pfKLVfVyVG/view?usp=drive_link" }
+            ]
+        },
+        {
+            title: "MWIII Public Beta",
+            img: "images/mwiii/beta.jpg",
+            tags: "27GB · Beta",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1-j7qluEM_xhLhbGsKZfG5kLBsVKlGHYM/view?usp=sharing" }
+            ]
+        }
+    ],
+    bo6: [
+        {
+            title: "Black Ops 6 Singleplayer 11.1",
+            img: "images/bo6/sp.jpg",
+            tags: "48GB · Singleplayer Only",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1SWNHFkQ6HeA7CsqzkByTCHplpjFi6JU_/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Black Ops 6 PS4 Alpha",
+            img: "images/bo6/alpha.jpg",
+            tags: "25GB · Alpha (JB Playstaion Only)",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1sJ8bDmiXX8BJuiG3SoYMypGtwEZlYEJu/view?usp=sharing" }
+            ]
+        }
+    ],
+    iw: [
+        {
+            title: "(Latest) Retail - IW7-Mod",
+            img: "images/iw/retail.jpg",
+            tags: "116GB · Full Build",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1DEIYFYhMqTxgNwzsdSYE4pbj4027angX/view?usp=sharing" }
+            ]
+        }
+    ],
+    other: [
+        {
+            title: "Black Ops 1",
+            img: "images/other/bo1.jpg",
+            tags: "9GB · Retail",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1iDXEykXOllNYEZdtcxgFnhU-kZwE5bEj/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Black Ops II",
+            img: "images/other/bo2.jpg",
+            tags: "31GB · Retail",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1sZHe1087Tv4_q4APWrW1CHtuXj8x379y/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Black Ops III",
+            img: "images/other/bo3.jpg",
+            tags: "100GB · Retail",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1qDTuQi6jyau2TwBdIuMLWyHG_0745_9l/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "NX1 (Future Warfare) (2012)",
+            img: "images/other/nx1.jpg",
+            tags: "13GB· Xbox 360 · Unreleased Demo",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1-ZTI0IEzgLZ2jwncpYhVxIekYPN-wi3T/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "World At War",
+            img: "images/other/waw.jpg",
+            tags: "13GB· Retail",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1vP4yBKGWXhfadvIbgNmNIXaypLqNzGIi/view?usp=sharing" }
+            ]
+        },
+        {
+            title: "Modern Warfare 2 (Alpha)",
+            img: "images/other/mw2.jpg",
+            tags: "4GB· Xbox 360",
+            links: [
+                { label: "Download", url: "https://drive.google.com/file/d/1XJtV8RF_zs2RgkIDlwQ-TO56TG1bzfK8/view?usp=sharing" }
+            ]
+        }
+    ]
+};
